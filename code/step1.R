50233e2979922e3a3ed2551fc368ae3898f607ee
taba3967 <taba3967@lxibm103>
2019-04-01 14:26:13 +0300

    step1 completed, Rmd to be made

--- code/step1.R
+++ code/step1.R
@@ -1,4 +1,5 @@
 library(minfi)
+library(annmatrix)
 
 # 1. Annotation prep
 
@@ -30,4 +31,48 @@ met_detectionP = met_detectionP[,good_samples]
 
 met_data = preprocessFunnorm(met_data)
 
-# 5.
+# 5. Removal of probes that have > 0.01 
+
+# since met_data is smaller now
+met_detectionP <- met_detectionP[
+	match(rownames(met_data), rownames(met_detectionP)),
+	match(colnames(met_data), colnames(met_detectionP))
+]
+
+good_probes <- rowMeans(met_detectionP > 0.01) < 0.01
+
+met_data <- met_data[good_probes, ]
+
+# 6. Remove CH probes and SNP probes
+
+met_data <- dropMethylationLoci(met_data)
+
+# 7. Extract the 3 tables from the prepared object file
+
+met_annmat <- annmatrix(x = getBeta(met_data),
+	rann = getAnnotation(met_data),
+	cann = pData(met_data))
+
+saveRDS(met_annmat, "output/step1_annmat.RDS")
+
+# 8. Implement the IAC outlier removal method
+
+#removal of samples with IAC which deviate from the mean by 2sd
+
+outlier_no = 100
+sd_cutoff = -3
+while(outlier_no > 0){
+	cor_mat <- cor(met_annmat)
+	meancorrs <- scale(colMeans(cor_mat))
+	stopifnot(sum(rownames(meancorrs) != colnames(met_annmat)) == 0)
+	met_annmat <- met_annmat[,meancorrs > sd_cutoff]
+	outlier_no <- sum(meancorrs < sd_cutoff)
+}
+
+# 9. Add a short quality control step
+
+#qc by relation to island
+cpg_means <- rowMeans(met_annmat)
+names(cpg_means) <- met_annmat@Relation_to_Island
+
+saveRDS(met_annmat, "output/final_annmat.RDS")
